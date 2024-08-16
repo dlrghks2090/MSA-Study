9be7c95093bdf10d33813ea70d79c9747f7cd485
Kim IkHwan <dlrghks2090@gmail.com>
2024-08-16 20:41:41 +0900

    Feat: Spring Cloud Netflix Eureka 를 적용하여 세팅

--- ecommerce/src/main/java/com/example/ecommerce/EcommerceApplication.java
+++ ecommerce/src/main/java/com/example/ecommerce/EcommerceApplication.java
@@ -2,9 +2,11 @@ package com.example.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-public class EcommerceApplication {
+@EnableEurekaServer     // 해당 프로젝트를 서비스디스커버리로서 수행한다.
+public class  EcommerceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EcommerceApplication.class, args);
